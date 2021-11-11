cf663ff85eb57cd42dfc42c456a5acf30fe5143d
kwonhaeun <wooyoung_000@naver.com>
2021-11-11 11:32:45 +0900

    초간단

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "Hello, Heroku"
+    return render_template("index.html")
+
+
+@app.route("/profile")
+def profile():
+    like_foods = ["떡볶이", "궁중떡볶이", "치즈떡볶이", "라볶이"]
+    return render_template("profile.html", like_foods=like_foods)
+
+
+@app.route("/posts")
+def post_list():
+    return render_template("post_list.html")
